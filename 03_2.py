7dee9ba3949fb7117ae97a10fcdc1164d013008d
Chris Arnott <christopher.arnott@stir.ac.uk>
2022-12-03 21:28:00 +0000

    tidying and using unpack operator *

--- 03_2.py
+++ 03_2.py
@@ -9,22 +9,13 @@ priority={'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l
          'A':27,'B':28,'C':29,'D':30,'E':31,'F':32,'G':33,'H':34,'I':35,'J':36,'K':37,'L':38,
          'M':39,'N':40,'O':41,'P':42,'Q':43,'R':44,'S':45,'T':46,'U':47,'V':48,
          'W':49,'X':50,'Y':51,'Z':52}
-
 sumbo=0         
 for i in range(0,len(lines),3):
     bagA=lines[i]
     bagB=lines[i+1]
     bagC=lines[i+2]
-    setA=set()
-    setB=set()
-    setC=set()
-    for char in bagA:
-        setA.add(char)
-    for char in bagB:
-        setB.add(char)
-    for char in bagC:
-        setC.add(char)
-    #print(min(setA.intersection(setB,setC)))
+    setA=set([*bagA])
+    setB=set([*bagB])
+    setC=set([*bagC])
     sumbo+=priority[min(setA.intersection(setB,setC))]
-
 print(sumbo)
