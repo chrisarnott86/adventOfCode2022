6d58b7de6a0776ecce9c10e1a9f3c0da7dc7f42c
chrisarnott86 <christopher.arnott@stir.ac.uk>
2022-12-04 15:30:04 +0000

    tidy

--- 01_2.py
+++ 01_2.py
@@ -1,19 +1,11 @@
-#from numpy import loadtxt
-
-#file=open('input1.txt','r')
-#file=open('input1-test.txt','r')
-#data = loadtxt('input.txt',dtype='int')
-#data = file.read()
 with open('input1.txt','r') as file:
 #with open('input1-test.txt','r') as file:
     lines = file.readlines()
     lines = [line.strip() for line in lines]
 
-#print(lines)
 bagtot=0
 bags=[]
 for line in lines:
-    #print(line)
     if line!="":
         bagtot+=int(line)
     else:
@@ -23,13 +15,3 @@ for line in lines:
 bags.sort(reverse=True)
 #print(bags)
 print(sum(bags[0:3]))
-
-#count = 0
-#temp1 = data[0]
-#for i in data:
-#    temp2 = i
-#    if temp2>temp1:
-#        count+=1
-#    temp1=i
-
-#print(count)
